82cbcc94943e6b7e016647515fdc8e009249cbd7
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 12:32:37 +0900

    Add files via upload

---
+++ Return.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Return {
+
+	public static void main(String[] args) {
+		int num = multi(7, 8);
+		System.out.println(num);
+
+	}
+
+
+	public static int multi(int x, int y) {
+		return x * y;
+
+	}
+}
